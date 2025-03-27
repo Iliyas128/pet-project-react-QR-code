99f1e9c7c4179068d2a209a254ef3e8c62a4c1ef
Iliyas128 <Weking128@gmail.com>
2025-03-27 16:54:27 +0500

    Layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Layout } from './layout'
+import { Layout } from './Layout'
 import { BrowserRouter } from 'react-router-dom'
 
 
